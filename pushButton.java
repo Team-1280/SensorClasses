167d2393fb4ccbac7e3d720c29fefb2cc92200b0
DragonXC2 <dragonaxc@gmail.com>
2019-01-07 15:18:33 -0800

    Added Button Class

---
+++ pushButton.java
@@ -0,0 +1,17 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class pushButton{
+    int port;
+    DigitalInput button;
+    public pushButton(int port){
+        this.port = port;
+        button = new DigitalInput(port);
+    }
+    public int showPort(){
+        return this.port;
+    }
+    public boolean get(){
+        return button.get();
+    }
+}
