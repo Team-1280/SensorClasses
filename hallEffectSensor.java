5bbd782ad62f286afc96e03b27204aa4a6aa79a2
DragonXC2 <dragonaxc@gmail.com>
2019-01-07 14:07:20 -0800

    Hall Effect Sensor

---
+++ hallEffectSensor.java
@@ -0,0 +1,17 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class hallEffectSensor{
+    int port;
+    DigitalInput sensor;
+    public hallEffectSensor(int port){
+        this.port = port;
+        sensor = new DigitalInput(port);
+    }
+    public int showPort(){
+        return this.port;
+    }
+    public boolean get(){
+        return sensor.get();
+    }
+}
