0ca2903c4fa5a290191cf655538260f738d6295e
Spencer Level <cobaltwars@gmail.com>
2019-01-07 15:25:09 -0800

    Added more functionality to UltrasonicAnalogSensor, still broken tho

--- UltrasonicAnalogSensor.java
+++ UltrasonicAnalogSensor.java
@@ -3,16 +3,24 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 public class UltrasonicAnalogSensor {
     int port;
+    double distance;
+    double voltInches;
     DigitalInput sensor;
   
-    public void DiffuseSensor(int port) {
+    public void UltrasonicSensor(int port) {
         this.port = port;
         sensor = new DigitalInput(port);
+        // Sensor and DigitalInput are screwed up and we need to go over them again
     }
     public int getPort() {
         return this.port;
     }
-    public boolean get() {
-        return sensor.get();
+    public void setVoltInches(double voltInches) {
+    }
+    public double getDistance() {
+        distance = voltInches*voltage
+        // Get the voltage and multiply by volt-inches to get the distance!
+        // Remember to set the distance!
+        return distance;
     }
 }
