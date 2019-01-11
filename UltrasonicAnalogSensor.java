a3db62443cab86c26e39228f1921c083c51e0e4b
Spencer Level <cobaltwars@gmail.com>
2019-01-10 22:44:46 -0800

    Fixed UltrasonicAnalogSensor to use Centimeters instead of Inches to fit with MaxBotix documentation

--- UltrasonicAnalogSensor.java
+++ UltrasonicAnalogSensor.java
@@ -4,29 +4,29 @@ import edu.wpi.first.wpilibj.AnalogInput;
 public class UltrasonicAnalogSensor {
     int port;
     double distance;
-    double inchesPerVolt;
+    double cmPerVolt;
     double voltage;
     AnalogInput sensor;
   
-    public void UltrasonicSensor(int port, double inchesPerVolt) {
+    public void UltrasonicSensor(int port, double cmPerVolt) {
         this.port = port;
-        this.inchesPerVolt = inchesPerVolt;
+        this.cmPerVolt = cmPerVolt;
         sensor = new AnalogInput(port);
     }
     public int getPort() {
         return this.port;
     }
-    public void setInchesPerVolt(double inchesPerVolt) {
-        this.inchesPerVolt = inchesPerVolt;
+    public void setCmPerVolt(double cmPerVolt) {
+        this.cmPerVolt = cmPerVolt;
     }
-    public double getInchesPerVolt() {
-        return this.inchesPerVolt;
+    public double getCmPerVolt() {
+        return this.cmPerVolt;
     }
     public double getDistance() {
         try {
-        distance = inchesPerVolt * sensor.getVoltage();
+        distance = cmPerVolt * sensor.getVoltage();
         } catch (Exception e) {
-            System.out.println("Error with distance calculation! Make sure to setVoltInches and check if voltage is being input.");
+            System.out.println("Error with distance calculation! Make sure to check cmPerVolt is set and check if voltage is being input.");
         }
         // Distance is how many inches per volt multiplied by the voltage output by the sensor
         return distance;
