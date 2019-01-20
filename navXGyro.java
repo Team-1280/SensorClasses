951b5877db52af1c5ebdd5cf85d8dafb51cc9a14
Spencer Level <cobaltwars@gmail.com>
2019-01-20 10:05:51 -0800

    Fixed gyroscope a bit (note: port is screwed up a bit, read comments)

--- navXGyro.java
+++ navXGyro.java
@@ -7,24 +7,22 @@ import edu.wpi.first.wpilibj.SPI.Port;
 
 
 public class navXGyro {
-double bearing;
-public static int port;
-AHRS gyroscope;
-
+    public static int port;
+    AHRS gyroscope;
     public navXGyro(int kPort) {
         this.kPort = port;
-        gyroscope = new AHRS(SPI.Port port);
+        gyroscope = new AHRS(SPI.Port port); //Note: this is giving me a syntax error on 'port' so it might be screwed up, I don't see what's wrong though
     }
-    public void reset() {
+    public void reset() { //"Resets the Yaw gyro" (straight from API Documentation)
         gyroscope.reset();
     }
-    public void zero() {
+    public void zero() { //Zeroes yaw value to current angle 
         gyroscope.zeroYaw();
     }
-    public double getTotalAngle() {
+    public double getTotalAngle() { //returns accumulated angle
         return gyroscope.getAngle();
     }
-    public float getCurrentAngle() {
-        return gyroscope.getCompassHeading;
+    public float getCurrentAngle() { //returns current compass bearing (i.e. 0-360)
+        return gyroscope.getCompassHeading();
     }
 }
